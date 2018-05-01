e212c3888b78644beff5967dd60a3dd92dafcb3d
tkhkucd <takehiko.uchida@gmail.com>
2018-04-30 22:18:26 -0700

    chose your own Algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -28,17 +29,108 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
 
+# Naive_Bayes classifier TU
+from sklearn.naive_bayes import GaussianNB
 
-
-
-
-
-
+t0=time()
+print("Start GaussianNB")
+clf=GaussianNB()
+clf=clf.fit(features_train, labels_train)
+print("GaussianNB() finish fit() time=", round(time()-t0))
+pred=clf.predict(features_test)
+print("GaussianNB() finish predict() time=", round(time()-t0))
+acc=clf.score(features_test, labels_test)
+print("GaussianNB() finish score() accuracy=",acc," time=", round(time()-t0))
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+# SVM classifier TU
+from sklearn import svm
+t0=time()
+print("start SVM")
+clf=svm.SVC(kernel="rbf", C=100000.0)
+clf.fit(features_train, labels_train)
+print("SVM finish fit, time=", round(time()-t0))
+clf.predict(features_test)
+print("SVM finish predict, time=",round(time()-t0))
+acc= clf.score(features_test, labels_test)
+print("SVM finish score() accuracy=",acc," time=", round(time()-t0))
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
+# Decision tree
+from sklearn import tree
+t0=time()
+print("start Decision tree")
+clf=tree.DecisionTreeClassifier(min_samples_split=50)
+clf.fit(features_train, labels_train)
+print("Decision tree finish fit, time=", round(time()-t0))
+clf.predict(features_test)
+print("Decision tree finish predict, time=",round(time()-t0))
+acc= clf.score(features_test, labels_test)
+print("Decision tree finish score() accuracy=",acc," time=", round(time()-t0))
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
+# K nearest Neughbor
+from sklearn.neighbors import KNeighborsClassifier
+t0=time()
+print("start K nearest Neighbors")
+clf=KNeighborsClassifier()
+clf.fit(features_train, labels_train)
+print("K neighbors finish fit, time=", round(time()-t0))
+clf.predict(features_test)
+print("K neighbors finish predict, time=",round(time()-t0))
+acc= clf.score(features_test, labels_test)
+print("K Neighbors finish score() accuracy=",acc," time=", round(time()-t0))
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
+ #AbaBoost
+from sklearn.ensemble import AdaBoostClassifier
+t0=time()
+print("start Adaboost")
+clf=AdaBoostClassifier()
+clf.fit(features_train, labels_train)
+print("Ada boost finish fit, time=", round(time()-t0))
+clf.predict(features_test)
+print("Adaboost finish predict, time=",round(time()-t0))
+acc= clf.score(features_test, labels_test)
+print("AdaBoost finish score() accuracy=",acc," time=", round(time()-t0))
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
+#Ramdom forest
+from sklearn.ensemble import RandomForestClassifier
+t0=time()
+print("start RamdomForest")
+clf=RandomForestClassifier(n_estimators=50)
+clf.fit(features_train, labels_train)
+print("Randomforest finish fit, time=", round(time()-t0))
+clf.predict(features_test)
+print("Randomforest finish predict, time=",round(time()-t0))
+acc= clf.score(features_test, labels_test)
+print("Randomforest finish score() accuracy=",acc," time=", round(time()-t0))
+
+try:
+   prettyPicture(clf, features_test, labels_test)
+except NameError:
+   pass
