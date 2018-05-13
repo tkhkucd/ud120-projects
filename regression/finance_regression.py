a2f92036dd85f18e21dc6a223e190832c232f2a3
tkhkucd <takehiko.uchida@gmail.com>
2018-05-12 18:46:38 -0700

    Lesson7 Regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -2,7 +2,7 @@
 
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,47 +10,52 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 
 import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+#features_list = ["bonus", "long_term_incentive"]
+
+#data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn import linear_model
+reg=linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print("reg.coef_", reg.coef_)
+print("reg.intercept_", reg.intercept_)
+print("reg.score by train", reg.score(feature_train, target_train))
+print("reg.score by test", reg.score(feature_test, target_test))
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -63,7 +68,15 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
+    print ("Name Error")
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r")
+print("reg.coef_", reg.coef_)
+print("reg.intercept_", reg.intercept_)
+print("reg.score by train", reg.score(feature_train, target_train))
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
