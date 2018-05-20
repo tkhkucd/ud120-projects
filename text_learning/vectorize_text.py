0413897fb66f67f3f9f1d496fe6663c2939b1de3
tkhkucd <takehiko.uchida@gmail.com>
2018-05-19 20:19:01 -0700

    Lesson11, Text Learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,69 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        #if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            #print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            replaced_words=""
+            #for word in words.split():
+            #    replaced_words += word.replace("sara", "")
+            #    replaced_words = replaced_words.replace("shackleton", "")
+            #    replaced_words = replaced_words.replace("chris", "")
+            #    replaced_words = replaced_words.replace("germani", "")
+            #    if word=="sara":
+            #        replaced_words += words.replace("sara","")
+            #    elif word=="shackleton":
+            #        replaced_words += words.replace("shackleton","")
+            #    elif word=="chris":
+            #        replaced_words += words.replace("chris","")
+            #    elif word=="germani":
+            #        replaced_words += words.replace("germani","")
+            #    else:
+            #        replaced_words += word + " "
+
+            replaced_words = words.replace("sara", "")
+            replaced_words = replaced_words.replace("shackleton", "")
+            replaced_words = replaced_words.replace("chris", "")
+            replaced_words = replaced_words.replace("germani", "")
 
             ### append the text to word_data
+            word_data.append(replaced_words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name=="sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
-print "emails processed"
+print ("emails processed")
+print(word_data[152])
+
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+#pickle.dump( word_data, open("your_word_data.pkl", "w") )
+#pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit(word_data)
+bag_of_words = vectorizer.transform(word_data)
+features=vectorizer.get_feature_names()
+print(type(bag_of_words),type(features), type(features[0]), type(features[1]))
+print(features[0], features[1])
+print("len of features", len(features),"features[34597]", features[34597])
